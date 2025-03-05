021ce886f844a1734edf2c116280eb0e4f8cef2d
Nonetone <Nonetone@LAPTOP-VUQB3JL2>
2025-03-05 15:59:58 +0300

    Initial project setup from Spring Initializr

---
+++ src/main/java/com/mcp/mcp_file_processor/McpFileProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.mcp.mcp_file_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpFileProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpFileProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcp/mcp_file_processor/McpFileProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp.mcp_file_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpFileProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
